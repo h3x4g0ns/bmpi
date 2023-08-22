959b8c787058a0bc36455cfca724c14a52431af5
h3x4g0ns <agrawalekansh02@gmail.com>
2023-08-22 16:06:27 -0700

    basic dummy viz

--- viz/src/App.tsx
+++ viz/src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Sidebar extends Component {
+  render() {
+    return (
+      <div className="bg-gray-800 text-white w-64 h-screen p-4">
+        <h1 className="text-2xl font-semibold mb-4">Dashboard</h1>
+        <ul>
+          <li className="mb-2">Home</li>
+          <li className="mb-2">Analytics</li>
+          <li className="mb-2">Settings</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class MainContent extends Component {
+  render() {
+    return (
+      <div className="flex-1 p-4">
+        <h2 className="text-xl font-semibold mb-4">Welcome to the Dashboard!</h2>
+        <p>This is the main content area of your dashboard.</p>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="flex h-screen">
+        <Sidebar />
+        <MainContent />
+      </div>
+    );
+  }
 }
 
 export default App;
