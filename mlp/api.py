ba3a485ff6867f9301a9f6b5b2704629724a79c5
h3x4g0ns <agrawalekansh02@gmail.com>
2023-08-25 00:52:37 -0700

    logging cleanup

--- mlp/api.py
+++ mlp/api.py
@@ -1,27 +1,35 @@
 from flask import Flask, request, jsonify
 from config import Model
 from cache import ModelCache
+import logging
+import time
 
 app = Flask(__name__)
-
 cache = ModelCache()
 
 @app.route("/api/v1/simple_model", methods=["POST"])
 def predict():
-    try:
-        model_name = "simple_model"
-        model = cache.get(model_name)
-        if not model:
-            model = Model(model_name="simple_model")
-        data = request.json['data']
-        prediction = model(data)
-        response = {
-            'prediction': prediction
-        }
-        return jsonify(response), 200
+  try:
+    model_name = "simple_model"
+    model = cache.get(model_name)
+    if not model:
+        logging.warning(f"{model_name} not found in cache")
+        model = Model(model_name="simple_model")
+        cache.set(model_name, model)
+    logging.info(f"serving {model_name}")
+    
+    data = request.json['data']
+    start = time.time()
+    prediction = model(data)
+    duration = time.time() - start
+    logging.info(f"{model_name} pass took {duration}")
+    response = {
+        'prediction': prediction
+    }
+    return jsonify(response), 200
 
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+  except Exception as e:
+    return jsonify({'error': str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+  app.run(debug=True, host="0.0.0.0", port=8080)
